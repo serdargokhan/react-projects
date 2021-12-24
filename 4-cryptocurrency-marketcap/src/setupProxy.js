aef0ed5324bfe4e08bd464f29cac5d77f37550d0
serdargokhan <gokhan_bakirci_53@hotmail.com>
2021-12-24 12:45:39 +0300

    fixes cors problem

---
+++ 4-cryptocurrency-marketcap/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+module.exports = function (app) {
+    app.use(
+        '​/api',
+        createProxyMiddleware({
+            target: 'http://siteurl1.com',
+            changeOrigin: true,
+        })
+    );
+};
