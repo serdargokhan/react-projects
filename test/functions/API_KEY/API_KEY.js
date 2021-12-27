68a8d75345309c3f23a3b0cc856c3cef2a4bc1d9
serdargokhan <gokhan_bakirci_53@hotmail.com>
2021-12-27 11:15:30 +0300

    test project

---
+++ test/functions/API_KEY/API_KEY.js
@@ -0,0 +1,18 @@
+const process = require('process')
+const qs = require('qs')
+
+const handler = async function (event) {
+  // apply our function to the queryStringParameters and assign it to a variable
+  const API_PARAMS = qs.stringify(event.queryStringParameters)
+  console.log('API_PARAMS', API_PARAMS)
+  // Get env var values defined in our Netlify site UI
+
+  // TODO: customize your URL and API keys set in the Netlify Dashboard
+  // this is secret too, your frontend won't see this
+  const { API_SECRET } = process.env
+  const URL = `${API_SECRET}`
+
+  console.log('Constructed URL is ...', URL)
+}
+
+module.exports = { handler }
