d825b9a8d591a24a9a82de371f2883082caad6b7
serdargokhan <gokhan_bakirci_53@hotmail.com>
2022-01-27 11:43:43 +0300

    e-commerce website

---
+++ 6-e-commerce/components/UI/Footer.js
@@ -0,0 +1,20 @@
+import Twitter from "../../public/twitter.png";
+import LinkedIn from "../../public/linkedin.png";
+import Github from "../../public/github.png";
+import Image from "next/image";
+
+function Footer() {
+    return (
+        <footer className="grid w-full lg:w-11/12 md:w-11/12 lg:text-left mx-auto md:grid-cols-3 space-y-4 py-8 justify-center items-center bg-light-white">
+            <p className="text-light-gray text-center lg:text-left md:text-left md:mt-4 lg:mt-4">For development only.</p>
+            <a href="https://www.shopify.com/" target="_blank" rel="noopener" className="text-light-gray text-center">The Website</a>
+            <div className="flex gap-6 items-center justify-between md:justify-end">
+                <a href="https://www.linkedin.com/in/serdarrgokhann/" className="w-10 h-10" target="_blank" rel="noopener"><Image src={LinkedIn} alt="instagram" /></a>
+                <a href="https://twitter.com/serdarrgokhann" className="w-10 h-10" target="_blank" rel="noopener"><Image src={Twitter} alt="twitter" /></a>
+                <a href="https://github.com/serdargokhan" className="w-10 h-10" target="_blank" rel="noopener"><Image src={Github} alt="github" /></a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
